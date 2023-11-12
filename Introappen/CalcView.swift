afce4f0919ab9db346bbc49f74b29a28f50d170e
MikaelaNorrelokke <M.Norrelokke@outlook.com>
2023-11-12 11:21:49 +0100

    Uppgift 1

---
+++ Introappen/CalcView.swift
@@ -0,0 +1,18 @@
+//
+//  CalcView.swift
+//  Introappen
+//
+//  Created by Mikaela Nörrelökke on 2023-11-12.
+//
+
+import SwiftUI
+
+struct CalcView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalcView()
+}
